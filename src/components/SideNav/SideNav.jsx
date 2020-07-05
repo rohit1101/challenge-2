75e4e70fd96980a0f0d66a8ca684458afbfee85c
Rohit <srohit1101@gmail.com>
2020-07-05 12:12:33 +0530

    initial setup

---
+++ src/components/SideNav/SideNav.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./SideNav.module.css";
+
+export function SideNav() {
+  return (
+    <div>
+      <img src="../../assets/icons8-today-96.png" alt="" />
+    </div>
+  );
+}
