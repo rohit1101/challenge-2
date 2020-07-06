beecb21ff6152962dcea7eb88121831cc1fbe98d
Rohit <srohit1101@gmail.com>
2020-07-06 17:24:00 +0530

    working on sidebar

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -4,13 +4,17 @@ import logo from "../../assets/logo.png";
 import speedometer from "../../assets/speedometer.png";
 import graph from "../../assets/graph.png";
 import timerWhite from "../../assets/timer_white.png";
+import todo from "../../assets/icons8-today-96.png";
+import invoice from "../../assets/bill.png";
+import folder from "../../assets/folder.png";
 import { One } from "./One";
+import { Two } from "./Two";
 
 export function SideNav() {
   return (
     <div className={styles.nav}>
       <One speed={speedometer} graph={graph} timer={timerWhite} logo={logo} />
-      <One speed={speedometer} graph={graph} timer={timerWhite} logo={logo} />
+      <Two todo={todo} invoice={invoice} timer={timerWhite} folder={folder} />
     </div>
   );
 }
--- src/components/SideNav/Two.jsx
+++ src/components/SideNav/Two.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import styles from "./SideNav.module.css";
-
 import { Icon } from "./Icon";
 
-export function One({ speed, graph, timer }) {
+export function Two({ todo, invoice, folder }) {
   return (
     <>
       <div className={styles.dash}>
-        <Icon image={speed} />
+        <Icon image={todo} />
         <h2>Dashboard</h2>
       </div>
       <div className={styles.activ}>
-        <Icon image={graph} />
+        <Icon image={invoice} />
         <h2>Activity</h2>
       </div>
       <div className={styles.time}>
-        <Icon image={timer} />
+        <Icon image={folder} />
         <h2>TimeSheet</h2>
       </div>
     </>
